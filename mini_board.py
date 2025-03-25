38a7e83cc32cef1899e562cdaa5fe57b6b90cfea
Simi <nadavsimin@gmail.com>
2025-03-25 19:10:30 +0200

    added checking for winning

--- mini_board.py
+++ mini_board.py
@@ -43,3 +43,46 @@ class MiniBoard:
             return False
         else:
             self.board[row][collumn] = turn
+    
+    def check_win(self) -> bool:
+        """
+        This function checks if someone has won,
+        updates the 'won' value
+        and returns True or False if someone has won
+        """
+        #check for 3 in a row
+        for row in range(3):
+            if (not self.board[row][0] == Tile.EMPTY
+                and self.board[row][0] == self.board[row][1]
+                and self.board[row][0] == self.board[row][2]):
+                self.won = self.board[row][0]
+                return True
+        #check for 3 in a collumn
+        for collumn in range(3):
+            if (not self.board[0][collumn] == Tile.EMPTY
+                and self.board[0][collumn] == self.board[1][collumn]
+                and self.board[0][collumn] == self.board[2][collumn]):
+                self.won = self.board[0][collumn]
+                return True
+        #check for 3 in a diagonal
+        if (not self.board[0][0] == Tile.EMPTY
+            and self.board[0][0] == self.board[1][1]
+            and self.board[0][0] == self.board[2][2]):
+            self.won = self.board[0][0]
+            return True
+        if (not self.board[0][2] == Tile.EMPTY
+            and self.board[0][2] == self.board[1][1]
+            and self.board[0][2] == self.board[2][0]):
+            self.won = self.board[0][2]
+            return True
+        self.won = Tile.EMPTY
+        return False
+
+test = MiniBoard()
+test.place(0, 2, 1)
+test.place(1, 1, 1)
+test.place(2, 0, 1)
+test.check_win()
+print(test)
+test.won = 0
+print(test)
